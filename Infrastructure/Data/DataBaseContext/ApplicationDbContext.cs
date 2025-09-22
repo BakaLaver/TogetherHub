719f5ea4cc485da65013ad80e7109dce1b4cfe56
BakaLaver <panfilov.ivanman@yandex.ru>
2025-09-22 07:48:54 +0300

    2.5 Настройка хранения сущностей

--- Infrastructure/Data/DataBaseContext/ApplicationDbContext.cs
+++ Infrastructure/Data/DataBaseContext/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Data.DataBaseContext;
 using Domain.Model;
+using Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data.DataBaseContext
@@ -11,6 +12,24 @@ namespace Infrastructure.Data.DataBaseContext
         public ApplicationDbContext(DbContextOptions options): base (options) 
         {
         
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Topic>()
+                .Property(topic => topic.Id)
+                .HasConversion(
+                    id => id.Vulue,
+                    value => TopicId.Of(value)
+                );
+            modelBuilder.Entity<Topic>()
+                .OwnsOne(topic => topic.Location, location =>
+                {
+                    location.Property(l => l.City).HasColumnName("City");
+                    location.Property(l => l.Street).HasColumnName("Street");
+                });
         }
     }
 }
