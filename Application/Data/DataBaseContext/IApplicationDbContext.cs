290ad8d38257b9298919438730c28ea14676140c
BakaLaver <panfilov.ivanman@yandex.ru>
2025-09-22 07:31:07 +0300

    2.4 DbContext

---
+++ Application/Data/DataBaseContext/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Data.DataBaseContext
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Topic> Topics { get; }
+    }
+}
---
+++ Infrastructure/Data/DataBaseContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Application.Data.DataBaseContext;
+using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.DataBaseContext
+{
+    public class ApplicationDbContext:DbContext, IApplicationDbContext
+    {
+        public DbSet<Topic> Topics => Set<Topic>();
+
+        public ApplicationDbContext(DbContextOptions options): base (options) 
+        {
+        
+        }
+    }
+}
