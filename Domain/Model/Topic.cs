9645e1da7e3b32622af7f1c5c48fe5aac21895fe
BakaLaver <panfilov.ivanman@yandex.ru>
2025-09-22 06:57:35 +0300

    Добавьте файлы проекта.

---
+++ Domain/Model/Topic.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Abstractions;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public class Topic:Entity<TopicId>
+    {
+        public string Title { get; set; } = default!;
+        public DateTime? EventStrat { get; set; } = default!;
+        public string Summery { get; set; } = default!;
+        public string TopycType { get; set; } = default!;
+        public Location Location { get; set; } = default!;
+
+        public static Topic Create(
+            TopicId id, string title, DateTime eventStart,
+            string summery, string topicType, Location location)
+
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(title);
+            ArgumentException.ThrowIfNullOrWhiteSpace(summery);
+            ArgumentException.ThrowIfNullOrWhiteSpace(topicType);
+
+            Topic topic = new Topic
+            {
+                Id = id,
+                Title = title,
+                EventStrat = eventStart,
+                Summery = summery,
+                Location = location
+            };
+
+            return topic;
+        }
+    }
+}
---
+++ Domain/ValueObjects/TopicId.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Exeptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects
+{
+    public record TopicId
+    {
+        public Guid Vulue {  get;}
+
+        private TopicId(Guid value) 
+        {
+            this.Vulue = value;
+        }
+
+        public static TopicId Of(Guid value) 
+        {
+            if(value == Guid.Empty)
+            {
+
+                throw new DomainException("TopicId не может быть пустым.");
+            }
+            return new TopicId(value);
+        }
+    }
+
+}
