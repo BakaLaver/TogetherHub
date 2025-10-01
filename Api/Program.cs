f981942bbca9e04d97564d01c1bd11225a21c2ea
Иван <panfilov.ivanman@yandex.ru>
2025-10-01 06:54:18 +0300

    2.16 Логирование

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,6 +2,11 @@ using Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.AddConfiguration(
+        builder.Configuration.GetSection("Logging")
+    );
+builder.Logging.AddConsole();
+
 builder.Services
     .AddApiService(builder.Configuration)
     .AddInfrastructureService(builder.Configuration)
