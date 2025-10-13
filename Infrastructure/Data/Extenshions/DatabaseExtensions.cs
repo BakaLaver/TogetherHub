9e117af58074c25c64c142a1db392fb451c1ee7f
Иван <panfilov.ivanman@yandex.ru>
2025-10-13 05:36:47 +0300

    4.3 Наполнение пользователями

--- Infrastructure/Data/Extenshions/DatabaseExtensions.cs
+++ Infrastructure/Data/Extenshions/DatabaseExtensions.cs
@@ -1,5 +1,7 @@
-﻿using Infrastructure.Data.DataBaseContext;
+﻿using Domain.Security;
+using Infrastructure.Data.DataBaseContext;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.Data.Extenshions
@@ -14,13 +16,19 @@ namespace Infrastructure.Data.Extenshions
                 .GetRequiredService<ApplicationDbContext>();
             dbContext.Database.MigrateAsync().GetAwaiter().GetResult();
 
-            await SeedData(dbContext);
+            var manager = scope
+                .ServiceProvider
+                .GetRequiredService<UserManager<CustomIdentityUser>>();
+
+            await SeedData(dbContext, manager);
         }
 
-        private static async Task SeedData(ApplicationDbContext dbContext)
+        private static async Task SeedData(ApplicationDbContext dbContext, UserManager<CustomIdentityUser> manager)
         {
             await SeedTopicsAsync(dbContext);
+            await SeedIdentityUsersAsynck(dbContext, manager);
         }
+
 
         private static async Task SeedTopicsAsync(ApplicationDbContext dbContext)
         {
@@ -28,6 +36,16 @@ namespace Infrastructure.Data.Extenshions
             {
                 await dbContext.Topics.AddRangeAsync(InitialData.Topics);
                 await dbContext.SaveChangesAsync();
+            }
+        }
+        private static async Task SeedIdentityUsersAsynck(ApplicationDbContext dbContext, UserManager<CustomIdentityUser> manager)
+        {
+            if (!manager.Users.Any()) 
+            {
+                foreach(var users in InitialData.identityUsers) 
+                {
+                    await manager.CreateAsync(users, "1111");
+                }
             }
         }
     }
